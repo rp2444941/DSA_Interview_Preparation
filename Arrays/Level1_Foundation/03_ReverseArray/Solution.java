18fe6524f14e3fd962e073017746c014d35a73ac
Rudra Pratap <1000011653@dit.edu.in>
2026-02-21 20:15:16 +0530

    Solve Reverse Element (Level 1 Foundation)

---
+++ Arrays/Level1_Foundation/03_ReverseArray/Solution.java
@@ -0,0 +1,42 @@
+//Two Pointer / In-place swapping
+
+public class Solution {
+
+    // Method to reverse array using two pointers
+    public static void reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            // Swap elements
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            // Move pointers
+            left++;
+            right--;
+        }
+    }
+
+    // Main method to test
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        reverseArray(arr);
+
+        System.out.println("Reversed Array:");
+        printArray(arr);
+    }
+
+    // Helper method to print array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
