ed174d081adfd29957b3a60ff63f4b2d3024fa26
Rudra Pratap <1000011653@dit.edu.in>
2026-02-23 21:54:30 +0530

    Dsa problems

---
+++ src/arrays/level1/AverageOfArrayElements.java
@@ -0,0 +1,26 @@
+package arrays.level1;
+
+public class AverageOfArrayElements {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 9, -3, 7};
+
+        if (arr.length == 0) {
+            System.out.println("Array is empty, average not defined.");
+            return;
+        }
+
+        int sum = 0;
+
+        // Single pass traversal to compute sum
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        // Cast to double for correct decimal average
+        double average = (double) sum / arr.length;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+
+}
