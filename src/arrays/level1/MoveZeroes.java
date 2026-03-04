d24aa88013515d4049f9046350074ab1cd16d253
Rudra Pratap <1000011653@dit.edu.in>
2026-03-04 14:27:34 +0530

    move zeros at end

---
+++ src/arrays/level1/MoveZeroes.java
@@ -0,0 +1,28 @@
+package arrays.level1;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+
+        moveZeroes(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void moveZeroes(int[] arr) {
+        int index=0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i]!=0){
+                arr[index++]=arr[i];
+            }
+
+
+        }
+        while(index<arr.length){
+            arr[index++]=0;
+        }
+    }
+}
+
