74a37370227c5d799e88e72d169b811d65934c6c
Rudra Pratap <1000011653@dit.edu.in>
2026-03-19 21:27:37 +0530

    Container With Most Water

---
+++ src/arrays/two_pointer/Container_Water.java
@@ -0,0 +1,31 @@
+package arrays.two_pointer;
+
+public class Container_Water {
+    public static void main(String[] args) {
+        int[] arr={1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(arr));
+    }
+    public static int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            int area = minHeight * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
