c5931e153a7c15cf34d45c3b4cd86b712ccd7261
Rudra Pratap <1000011653@dit.edu.in>
2026-03-20 07:21:12 +0530

    three sum

--- src/arrays/two_pointer/TripleSum.java
+++ src/arrays/two_pointer/TripleSum.java
@@ -11,7 +11,7 @@ public class TripleSum {
         System.out.println(threeSum(arr));
     }
     public static List<List<Integer>> threeSum(int[] arr){
-          List<List<Integer>> result=new ArrayList<>();
+          List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(arr);
         for (int i = 0; i < arr.length-2; i++) {
             if(i>0 && arr[i]==arr[i-1]) continue;//skip duplicate
@@ -20,7 +20,7 @@ public class TripleSum {
             while(left<right){
                int sum=arr[i]+arr[left]+arr[right];
                if(sum==0){
-                   result.add(Arrays.asList(arr[i],arr[left],arr[right]));
+                   res.add(Arrays.asList(arr[i],arr[left],arr[right]));
                    while(left<right && arr[left]==arr[left+1]) left++;
                    while(left<right && arr[right]==arr[right-1]) right--;
                    left++;
@@ -33,7 +33,7 @@ public class TripleSum {
 
 
         }
-        return result;
+        return res;
     }
 
 
