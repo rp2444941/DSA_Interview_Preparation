c2b96e2b6538500ab32fd59eb3114f7f1518944e
Rudra Pratap <1000011653@dit.edu.in>
2026-03-27 19:29:16 +0530

    stack and queue basics

---
+++ src/Stack_Queue/Stack.java
@@ -0,0 +1,57 @@
+package Stack_Queue;
+
+public class Stack {
+    public static void main(String[] args) {
+        //LIFO---> plate ka stack -->> browser back history
+//operations
+     /*
+       push(x) → element dalo
+        pop() → top element hatao
+        peek() / top() → top dekho
+        isEmpty()
+
+        Push 10, 20, 30
+
+Top
+ |
+30
+20
+10
+
+pop() karoge:
+
+text
+
+Top
+ |
+20
+10
+        */
+        /*
+Queue kya hota hai?
+Queue = FIFO
+First In First Out
+
+Jo pehle aaya, wahi pehle niklega.
+
+Real life example:
+Ticket line
+Printer queue
+CPU scheduling
+Operations:
+offer(x) / enqueue(x) → element add karo
+poll() / dequeue() → front se hatao
+peek() → front dekho
+isEmpty()
+Diagram:
+text
+
+Front -> 10 20 30 <- Rear
+poll() ke baad:
+
+text
+
+Front -> 20 30 <- Rear
+*/
+    }
+}
