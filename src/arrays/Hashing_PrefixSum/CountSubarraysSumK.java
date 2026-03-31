6350e46403b2f7fa69a03fa77efa578caf33d13c
Rudra Pratap <1000011653@dit.edu.in>
2026-03-31 18:57:46 +0530

    Count Subarrays SumK optimal

--- src/arrays/Hashing_PrefixSum/CountSubarraysSumK.java
+++ src/arrays/Hashing_PrefixSum/CountSubarraysSumK.java
@@ -1,12 +1,30 @@
 package arrays.Hashing_PrefixSum;
-
+import java.util.*;
 public class CountSubarraysSumK {
     public static void main(String[] args) {
         int[] arr = {1, 1, 1};
         int k = 2;
         System.out.println(countSubarrays(arr, k));
+        System.out.println(countSubarrays1(arr, k)); // 2
     }
+    public static int countSubarrays1(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // important
+        int prefixSum = 0;
+        int count = 0;
 
+        for (int num : arr) {
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
     private static int countSubarrays(int[] arr, int k) {
         int n=arr.length;
         int count=0;
