be4ba1ade9c27023171189801d39105c187bf5cf
Rudra Pratap <1000011653@dit.edu.in>
2026-04-06 22:30:37 +0530

    two sum

--- src/arrays/level2/TwoSum.java
+++ src/arrays/level2/TwoSum.java
@@ -7,6 +7,11 @@ public class TwoSum {
 
         System.out.println(Arrays.toString(twoSum(num, target)));
 
+        int[] nums = {2, 7, 11, 15};
+//        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum1(nums, target)));
+
     }
     public static int[] twoSum(int[] num, int target) {
         int n = num.length;
@@ -21,5 +26,26 @@ public class TwoSum {
 
 
         return new int[]{-1, -1}; // just for safety
+    }
+
+    public static int[] twoSum1(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                int j = map.get(complement);
+                if (j < i) {
+                    return new int[]{j, i};
+                } else {
+                    return new int[]{i, j};
+                }
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1}; // safety
     }
 }
