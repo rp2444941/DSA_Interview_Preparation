4ee13f037566bdf4b276462c0219018f1ce7f99e
Rudra Pratap <1000011653@dit.edu.in>
2026-04-15 15:14:09 +0530

    Subarray Sum Equals K optimize approach

--- src/arrays/level2/subarraySum.java
+++ src/arrays/level2/subarraySum.java
@@ -1,10 +1,13 @@
 package arrays.level2;
 
+import java.util.HashMap;
+
 public class subarraySum {
     public static void main(String[] args) {
         int[] nums={1,2,3};
         int k=3;
         System.out.println(subarraySum1(nums,k));
+        System.out.println(subarraySum2(nums,3));
     }
     public static int subarraySum1(int[] nums, int k) {
         int count=0;
@@ -19,6 +22,27 @@ public class subarraySum {
                 }
 
             }
+
+        }
+        return count;
+    }
+    public static int subarraySum2(int[] nums, int k) {
+        int count=0;
+        int runningSum=0;
+        //hashmap:<prefix Sum,Frequency>
+        HashMap<Integer,Integer> map=new HashMap<>();
+        // base case:0 sum ek bar  aa chuka hai(empty subaaray ka sum hai)
+        map.put(0,1);
+        for (int i = 0; i <nums.length ; i++) {
+            //1. Current sum calculate karo
+            runningSum+=nums[i];
+            //2. check kro kya(runningsum-k) history me exist krta hai
+            int removeSum=runningSum-k;
+            if(map.containsKey(removeSum)){
+                count+=map.get(removeSum);
+            }
+            //3. current runningsum ko map me add/ update karo future ke liye
+            map.put(runningSum,map.getOrDefault(runningSum,0));
 
         }
         return count;
