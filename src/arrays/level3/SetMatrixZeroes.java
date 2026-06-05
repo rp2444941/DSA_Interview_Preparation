1132d35033826b93a6fd9dd488dc19496e7f4933
Rudra Pratap <1000011653@dit.edu.in>
2026-06-05 12:18:39 +0530

    set matrix zeroes

--- src/arrays/level3/SetMatrixZeroes.java
+++ src/arrays/level3/SetMatrixZeroes.java
@@ -93,31 +93,48 @@ public class SetMatrixZeroes {
 //        }
 //
 //    }
-    public static void setZeroes(int[][] matrix){
-        int n= matrix.length;
-        int m= matrix[0].length;
-        int col0=1;
-        for (int i = 0; i <n ; i++) {
-                if(matrix[i][0]==0){
-                    col0=0;
-                }
-                for (int j = 1; j < m; j++) {
-                    if(matrix[i][j]==0){
-                        matrix[i][0]=0;
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1;
+        // scan mark row and col 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <n ; j++) {
+                if(matrix[i][j]==0){
+                    // mark row 0
+                    matrix[i][0]=0;
+                    // mark col 0
+                    if(j!=0){
                         matrix[0][j]=0;
+                    }else{
+                        col0=0;
                     }
+                }
+                
+            }
+            
+        }
+        //  Step 2: Update inner matrix (reverse traversal)
+        for (int i = m-1; i >=1 ; i--) {
+            for(int j=n-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
 
                 }
-        }
-        // Step 2: Update matrix (reverse order)
-        for(int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 1; j--) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
             }
-            if (col0 == 0) {
-                matrix[i][0] = 0;
+
+        }
+        // Step 3: Update first row if needed
+        if(matrix[0][0]==0){
+            for (int j = 0; j < n; j++) {
+                matrix[0][j]=0;
+
+            }
+        }
+        if(col0==0){
+            for (int i = 0; i < m; i++) {
+                matrix[i][0]=0;
+
             }
         }
     }
